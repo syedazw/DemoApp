130ac37e83fab98dc844fb2aaea635a4e7b26c23
syedazw <syedazw@gmail.com>
2023-04-07 14:07:14 +0500

    Fifth Commit

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -5,7 +5,7 @@ export default function MainPage() {
     const buttonStyle = { color: "white", backgroundColor: "#041342" }
     return (
         <>
-            <p className="h3 text-center fw-bold pt-5">IMMEDIATE FIRST AID</p>
+            <p className="h3 text-center fw-bold pt-5">Immediate First Aid</p>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 ">

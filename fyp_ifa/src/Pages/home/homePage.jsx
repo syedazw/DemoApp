3d21aa212644352882bb94f186982d90a180b49b
Bilal-19 <57635732+Bilal-19@users.noreply.github.com>
2023-08-01 15:48:00 +0500

    improve patient homepage interface

--- fyp_ifa/src/Pages/home/homePage.jsx
+++ fyp_ifa/src/Pages/home/homePage.jsx
@@ -3,12 +3,10 @@ import Cardiogram from "../../Components/PlotECG";
 import { Outlet, Link } from "react-router-dom"
 import { usefirebase } from "../../context/firebase";
 import { getAuth } from "firebase/auth";
-import { getFirestore, collection, where, query, getDocs } from "firebase/firestore";
-
 
 export default function HomePage() {
     const auth = getAuth();
-   
+
     const firebase = usefirebase();
 
     // **************************    PATIENT DATA ***************************
@@ -41,7 +39,7 @@ export default function HomePage() {
         firebase.patMedData(userEmail)
             .then((matchingData) => {
                 setpatMed(matchingData);
-                console.log("medication",setpatMed)
+                console.log("medication", setpatMed)
             })
             .catch((error) => {
                 console.log("Error fetching medication data:", error);
@@ -56,18 +54,18 @@ export default function HomePage() {
         if (!currentUser) {
             return;
         }
-            const userEmail = currentUser.email;
-            firebase.patRepData(userEmail).then((matchingData) => {
-                setpatRep(matchingData);
-            })
-            .catch((error) =>{
+        const userEmail = currentUser.email;
+        firebase.patRepData(userEmail).then((matchingData) => {
+            setpatRep(matchingData);
+        })
+            .catch((error) => {
                 console.log("Error when Fetching Reports", error);
             })
-        
-    } ,[])
+
+    }, [])
 
 
-    
+
 
 
 
@@ -128,20 +126,11 @@ export default function HomePage() {
             {/* we can't import medication, recommendation and update component as its function is taking id as props for each patient */}
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm-12 col-md-6">
+                    <div className="col-sm-12 col-md-4">
                         <Cardiogram />
                     </div>
-                    <div className="col-sm-12 col-md-4">
 
 
-
-                    </div>
-                </div>
-            </div>
-
-            {/* creating a medication card */}
-            <div className="container-fluid">
-                <div className="row">
                     <div className="col-sm-12 col-md-3">
                         <div className="card mb-5" style={{ color: "white", backgroundColor: "#041342" }}>
                             <div className="card-body">
@@ -151,35 +140,28 @@ export default function HomePage() {
                                     {patMed.length > 0 && patMed[0].medications.map((medication, index) => (
                                         <li key={index}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-capsule mb-2 mx-2" viewBox="0 0 16 16"><path d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429l4.243 4.242Z" /></svg>
                                             {medication.data[0].medicineName}  ({medication.data[0].dosage})</li>
-                                    ))} 
+                                    ))}
                                 </ul>
                             </div>
                         </div>
                     </div>
 
                     <div className="col-sm-12 col-md-3">
-                                <div className="card mb-5" style={{ color: "white", backgroundColor: "#041342" }}>
-                                    <div className="card-body">
-                                        <h5 className="card-title">REPORTS</h5>
-                                        <hr className="border-5" style={{ color: "white" }} />
-                                        <ul className="list-unstyled">
-                                            {patRep.length > 0 &&
-                                                patRep[0].Reports.map((report) => (
-                                                    <li className="bi bi-file-text-fill me-2"> {report.testName}</li>
-                                                ))}
-                                        </ul>
-                                    </div>
-                                </div>
+                        <div className="card mb-5" style={{ color: "white", backgroundColor: "#041342" }}>
+                            <div className="card-body">
+                                <h5 className="card-title">REPORTS</h5>
+                                <hr className="border-5" style={{ color: "white" }} />
+                                <ul className="list-unstyled">
+                                    {patRep.length > 0 &&
+                                        patRep[0].Reports.map((report) => (
+                                            <li className="bi bi-file-text-fill me-2"> {report.testName}</li>
+                                        ))}
+                                </ul>
                             </div>
-                    
-
+                        </div>
+                    </div>
                 </div>
             </div>
-
-
-
-
-
         </>
     )
 }
